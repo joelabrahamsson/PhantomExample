0635ed51df41a6a7f22d7241cc4f551031e26aaa
joelabrahamsson <mail@joelabrahamsson.com>
2010-07-11 19:08:51 +0200

    Create the project, first commit

---
+++ ConfigurableModule/Configuration.cs
@@ -0,0 +1,29 @@
+﻿using System.Configuration;
+
+namespace ConfigurableModule
+{
+    public class Configuration : ConfigurationSection  
+    {
+        public static Configuration GetConfiguration()
+        {
+            Configuration configuration = 
+                ConfigurationManager
+                .GetSection("myConfigurableModule")
+                as Configuration;
+
+            if (configuration != null)
+                return configuration;
+
+            return new Configuration();
+        }
+
+        [ConfigurationProperty("message", IsRequired = false)]
+        public string Message
+        {
+            get
+            {
+                return this["message"] as string;
+            }
+        }
+    }
+}
